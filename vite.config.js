e4596f49a1bb5734e4f5c0aa1050cb8173c90028
MinsungKxm <minsungk101@gmail.com>
2026-08-13 11:16:33 -0400

    @ent to the old version

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-        tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
+
+server: {
+  proxy: {
+    '/login': 'http://localhost:3000',
+    '/register': 'http://localhost:3000',
+    '/session': 'http://localhost:3000',
+    '/logout': 'http://localhost:3000',
+    '/events': 'http://localhost:3000',
+    '/tasks': 'http://localhost:3000',
+    '/deadlines': 'http://localhost:3000',
+    '/schedule': 'http://localhost:3000',
+  },
+}
 })
